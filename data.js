4df198f5f990c59e4d350b410f9985b3d20f9800
Axel Blaze <contentstudio1235@gmail.com>
2026-01-13 20:00:31 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+const projects = [
+  {
+    id: "stratos-network",
+    name: "Stratos Network",
+    category: ["DePIN"],
+    intro: "Building the next generation of decentralized data mesh for #Web3 #DePIN",
+    website: "https://www.thestratos.org",
+    twitter: "https://x.com/Stratos_Network",
+    status: "Upcoming",
+    round: "Not started",
+    date: "30 December 2024",
+    icon: "icons/stratos-network.png"
+  },
+  {
+    id: "push-protocol",
+    name: "Push Protocol",
+    category: ["Wallet"],
+    intro: "The communication layer of Web3. Powering decentralized notifications.",
+    website: "https://push.org",
+    twitter: "https://x.com/pushprotocol",
+    status: "Live",
+    round: "Seed",
+    date: "2024",
+    icon: "icons/push-protocol.png"
+  }
+];
